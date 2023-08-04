539a04bc6ad2558cc4a31c098bb7e8c2348a14f9
Oauthus42 <var.verdandi@gmail.com>
2023-08-04 23:02:07 +0300

    Базовые параметры

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 if(window.innerWidth > 800) {
     renderer.shadowMap.enabled = true;
-}
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.shadowMap.needsUpdate = true;
+};
+
+document.body.appendChild(renderer.domElement);
